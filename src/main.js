1660c88d333c730c8f59031ff39094c54f3f9207
sayhoChun <fishcreek@naver.com>
2019-01-07 12:03:43 +0900

    init)

---
+++ src/main.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store';
+
+Vue.config.productionTip = false
+
+// dist mode
+// Vue.config.devtools = false
+
+//dev mode
+Vue.config.devtools = true
+
+new Vue({
+  router, store,
+  render: h => h(App)
+}).$mount('#app')
+
+
